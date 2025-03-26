3d41a0a0f710357b6d88022b016942fb4c3d9401
furaoferrenho <scardozodacunha@gmail.com>
2025-03-26 17:39:25 -0300

    atualização de diretorio
    incompleta

--- dispositivo/code.js
+++
@@ -1,6 +0,0 @@
-         alert('Esse site é feito por um aluno do encino medio por tento, Não há o trabalho de portalo para um celular, se possivel abra isto em um computador');
-
-document.getElementById('enviar').onclick = function porta(){
-	let resutado = "funciona";
-};
-
