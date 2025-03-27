1daa520cbf9dbb05195523fe7abd03443521495b
furaoferrenho <scardozodacunha@gmail.com>
2025-03-27 15:47:57 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
          alert('Esse site é feito por um aluno do encino medio por tento, Não há o trabalho de portalo para um celular, se possivel abra isto em um computador');
 
 const porta = () =>{
-const pr = document.getElementById('porcentagem').value;
-const pbi = document.getElementById('pib').value;
-const j = document.getElementbyid('pessoas').value;
+const pr = document.getElementById("porcentagem").value;
+const pbi = document.getElementById("pib").value;
+const j = document.getElementbyid("pessoas").value;
 
 const cpib = pib * (pr / 100);
 const ppc = cpib / j;
