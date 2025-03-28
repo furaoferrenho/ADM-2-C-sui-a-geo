bff9a383f86fadecda4f390545f0d89aa3f8e0b3
furaoferrenho <scardozodacunha@gmail.com>
2025-03-28 20:07:20 -0300

    Update code.j

--- code.js
+++ code.js
@@ -1,14 +1,14 @@
          alert('Esse site é feito por um aluno do ensino medio por tanto, Não tive o trabalho de portalo para um celular, se possível que apresente alguns problemas pelo dispositivo movel');
 
 const porta = () =>{
-const pr = document.getElementById("porcentagem").value;
-const pbi = document.getElementById("pib").value;
-const j = document.getElementById("pessoas").value;
+const pr =float(document.getElementById("porcentagem").value;);
+const pbi =float(document.getElementById("pib").value;);
+const j = float(document.getElementById("pessoas").value;);
 
 const cpib = pib * (pr / 100);
 const ppc = cpib / j;
 
-return document.getelementbyid("resultado").innerhtml = "pibc é ${cpib} </p> pib percapita é ${ppc}";
+return document.getelementbyid("resultado").InnerHTML = "pibc é ${cpib} </p> pib percapita é ${ppc}";
          alert("foi?");
 };
 
