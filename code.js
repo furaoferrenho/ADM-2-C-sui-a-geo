1cddbcd10d94f46931e9ccb49907b3f5b7610323
furaoferrenho <scardozodacunha@gmail.com>
2025-03-28 20:20:53 -0300

    codigo temporariamente desativado
    por um erro meu, quebrei o codigo todo, temporariamente desativado

--- code.js
+++ code.js
@@ -1,16 +1,2 @@
-
-
-alert('Esse site é feito por um aluno do ensino médio portanto, não tive o trabalho de portá-lo para um celular, se possível que apresente alguns problemas pelo dispositivo móvel.');
-
-const porta = () => {
-    const pr = parseFloat(document.getElementById("porcentagem").value);
-    const pib = parseFloat(document.getElementById("pib").value);
-    const j = parseFloat(document.getElementById("pessoas").value);
-
-    const cpib = pib * (pr / 100);
-    const ppc = cpib / j;
-
-    document.getElementById("resultado").innerHTML = `pibc é ${cpib} </p> pib percapita é ${ppc}`;
-    alert("foi?");
-};
+alert("o codigo foi temporariamente deletado");
 
