a70b55a84d47073d643622cfaca23d4b96789667
furaoferrenho <scardozodacunha@gmail.com>
2025-03-28 20:12:45 -0300

    usei ia, para fazer, tristeza

--- code.js
+++ code.js
@@ -1,14 +1,16 @@
-         alert('Esse site é feito por um aluno do ensino medio por tanto, Não tive o trabalho de portalo para um celular, se possível que apresente alguns problemas pelo dispositivo movel');
 
-const porta = () =>{
-const pr =float(document.getElementById("porcentagem").value;);
-const pbi =float(document.getElementById("pib").value;);
-const j = float(document.getElementById("pessoas").value;);
 
-const cpib = pib * (pr / 100);
-const ppc = cpib / j;
+alert('Esse site é feito por um aluno do ensino médio portanto, não tive o trabalho de portá-lo para um celular, se possível que apresente alguns problemas pelo dispositivo móvel.');
 
-return document.getelementbyid("resultado").InnerHTML = "pibc é ${cpib} </p> pib percapita é ${ppc}";
-         alert("foi?");
+const porta = () => {
+    const pr = parseFloat(document.getElementById("porcentagem").value);
+    const pib = parseFloat(document.getElementById("pib").value);
+    const j = parseFloat(document.getElementById("pessoas").value);
+
+    const cpib = pib * (pr / 100);
+    const ppc = cpib / j;
+
+    document.getElementById("resultado").innerHTML = `pibc é ${cpib} </p> pib percapita é ${ppc}`;
+    alert("foi?");
 };
 
