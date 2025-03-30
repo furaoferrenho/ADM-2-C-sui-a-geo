29d8ff49e692153840dfc5b41c80d173eb6c5260
furaoferrenho <scardozodacunha@gmail.com>
2025-03-30 18:41:57 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
         alert('Esse site é feito por um aluno do ensino medio por tento, se possivel abra isto em um computador poís ele pode ter problemas de compatibilidade');
 
-function porta() = {
+function porta(){
 const pr = document.getElementById('porcentagem').value;
 const pbi = document.getElementById('pib').value;
 const j = document.getElementbyid('pessoas').value;
