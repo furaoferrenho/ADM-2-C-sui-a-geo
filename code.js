928b85bb6e25cd9198937a34703e9d8dbc5dce2b
furaoferrenho <scardozodacunha@gmail.com>
2025-03-30 18:16:08 -0300

    code.js de volta ao ar

--- code.js
+++ code.js
@@ -1,2 +1,15 @@
-alert("o codigo foi temporariamente deletado");
+        alert('Esse site é feito por um aluno do encino medio por tento, Não há o trabalho de portalo para um celular, se possivel abra isto em um computador');
+
+const porta = () =>{
+const pr = document.getElementById('porcentagem').valve;
+const pbi = document.getElementById('pib').valve;
+const j = document.getElementbyid('pessoas').valve;
+
+const cpib = pib*(pr/100);
+const ppc = cpib/j ;
+
+alert('pr');
+};
+
+
 
