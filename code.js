edbe0671caad5cb7d0ff5721fda0d17329feff47
furaoferrenho <scardozodacunha@gmail.com>
2025-04-17 21:10:36 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
         alert('Esse site é feito por um aluno do ensino medio por tento, se possivel abra isto em um computador poís ele pode ter problemas de compatibilidade');
 
 function porta(){
-var pr = float(document.getElementById('porcentagem').value);
-var pbi = float(document.getElementById('pib').value);
-var j = float(document.getElementbyid('pessoas').value);
+var pr = parseFloat(document.getElementById('porcentagem').value);
+var pbi = parseFloat(document.getElementById('pib').value);
+var j = parseFloat(document.getElementById('pessoas').value);
 
 var cpib = pib*(pr/100);
 var ppc = cpib/j ;
